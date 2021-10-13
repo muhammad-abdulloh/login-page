fe50c3a76a0356cd3c10bb6bc5b78989e2c33462
CleverCoder <clevercoderr@gmail.com>
2021-10-13 07:06:33 +0500

    REgistration qismi uchun homaki qolip tayyorllandi

---
+++ registration.py
@@ -0,0 +1,62 @@
+
+import os
+class User:
+    def __init__(self):
+        self.username = None
+        self.email = None
+        self.password = None
+
+    def entering(self):
+        self.clear_console()
+        print("""
+        Tizimga hush kelibsiz:
+        
+        1. SignIn
+        2. SignUp
+        """)
+
+        enter = input("Tanlash [1/2]: ").strip()
+        while enter not in ['1', '2'] or enter == "":
+            self.clear_console()
+            enter = input("Tanlash [1/2]: ").strip()
+
+        if enter == '1':
+            self.signin()
+        else:
+            self.signup()
+
+    def signin(self):
+        print("Bu login qismi")
+        pass
+
+    def signup(self):
+        self.clear_console()
+        print("Bu registration qismi")
+
+        self.input_username = input("Username ni kiriting: ").strip()
+        while self.input_username == "":
+            self.clear_console()
+            print("Invalid user name ")
+            self.input_username = input("User name ni qayta kiriting: ").strip()
+
+        self.input_email = input("Emailingizni kiriting: ").strip()
+        while self.input_email == "":
+            self.clear_console()
+            print("Invalid email")
+            self.input_email = input("Emailingizni kiriting: ").strip()
+
+        self.input_password = input("Passwordingizni kiriting: ").strip()
+        while self.input_password == "":
+            self.clear_console()
+            print("Invalid password please repeat input")
+            self.input_password = input("Passwordingizni kiriting: ").strip()
+
+
+
+    def clear_console(self):
+        os.system("cls")
+
+
+user = User()
+
+user.entering()
